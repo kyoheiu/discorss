28b3261b4732f5f048d3cab471532e862ffee143
Kyohei Uto <im@kyoheiu.dev>
2023-11-16 06:26:08 +0900

    Add single ParseFeed test

--- test/feed_test.go
+++ test/feed_test.go
@@ -1,7 +1,38 @@
 package feed_test
 
-import "testing"
+import (
+	"context"
+	"testing"
+	"time"
 
-func TestFeed(t *testing.T) {
-	t.Error("Test.")
+	"github.com/kyoheiu/discorss/feed"
+	"github.com/mmcdole/gofeed"
+)
+
+func TestParseFeed(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	feeds := feed.SetFeedList()
+
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURLWithContext(feeds[0], ctx)
+	if err != nil {
+		t.Error("Cannot get or parse feed")
+	}
+	items := feed.Items
+	for _, item := range items {
+		if item.PublishedParsed == (*time.Time)(nil) {
+			t.Log("Cannot get published date: ", item.Title)
+			continue
+		} else if item.PublishedParsed.Before(time.Now().Add(time.Duration(-24) * time.Hour)) {
+			t.Log("Too old: ", item.Title)
+			continue
+		} else if item.PublishedParsed.After(time.Now().Add(time.Duration(24) * time.Hour)) {
+			t.Log("Too new: ", item.Title)
+			continue
+		}
+
+		t.Error(item.Title)
+	}
 }
